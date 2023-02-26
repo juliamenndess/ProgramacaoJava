2eb8d44facd215ec02165255cf45887e6bb1dcd7
Julia Mendes <juulliiamendes@gmail.com>
2023-02-26 11:00:07 -0300

    Envio da resolução

--- Ativade1/OrdenarCrescente/resolucao.java
+++ Ativade1/OrdenarCrescente/resolucao.java
@@ -5,8 +5,33 @@ import java.util.Scanner;
 public class resolucao {
     public static void main(String[] args){
         Scanner in = new Scanner(System.in);
-        int[] preencher = new int[3];
+        int a, b, c;
+        int menor, medio, maior;
 
+        System.out.println("Informe o primeiro número: ");
+        a = in.nextInt();
+
+        System.out.println("Informe o segundo numero: ");
+        b = in.nextInt();
+
+        System.out.println("Informe o terciero número");
+        c = in.nextInt();
+
+        if(a < b && b < c){
+            System.out.println( a + "-"  + b + "-" + c);
+        }else if(a < c && c < b){
+            System.out.println( a + "-"  + c + "-"  +b);
+        }else if(c < a && a < b){
+            System.out.println( c + "-"  + a + "-"  +b);
+        }else if(c < b && b < a){
+            System.out.println( c + "-"  + b + "-"  +a);
+        }else if(b < a && a < c){
+            System.out.println(b + "-"  + a +  "-"  + c);
+        }else if(b < c && c < a){
+            System.out.println(b + "-"  + c + "-"  + a);
+        }
         
+        }
+
     }
-}
+
